e4e93535a673de625ecc121571f24b139c4d1df6
sammy <sammy@fiteen.(none)>
2010-08-28 19:45:31 -0700

    In swap performance test for 8GB machine

---
+++ IN_Swap_Performance_Test.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+echo ./redis-cli CREATE TABLE bigrow \(id int primary key, field TEXT\)
+./redis-cli CREATE TABLE bigrow \(id int primary key, field TEXT\)
+
+# INSERT INSERT INSERT INSERT INSERT INSERT INSERT  INSERT INSERT INSERT
+# INSERT INSERT INSERT INSERT INSERT INSERT INSERT  INSERT INSERT INSERT
+PNUM=20000000
+DUMP=100000
+
+echo taskset -c 1 ./redis-benchmark -n $PNUM -r $PNUM -B -X $DUMP
+taskset -c 1 ./redis-benchmark -n $PNUM -r $PNUM -B -X $DUMP
+echo ./redis-cli DESC bigrow
+./redis-cli DESC bigrow
+echo free -m
+free -m
+
+echo taskset -c 1 ./redis-benchmark -n $PNUM -r $PNUM -B -X $DUMP -NR $PNUM
+taskset -c 1 ./redis-benchmark -n $PNUM -r $PNUM -B -X $DUMP -NR $PNUM
+echo ./redis-cli DESC bigrow
+./redis-cli DESC bigrow
+echo free -m
+free -m
+
+# SELECT SELECT SELECT SELECT SELECT SELECT SELECT SELECT SELECT SELECT 
+# SELECT SELECT SELECT SELECT SELECT SELECT SELECT SELECT SELECT SELECT 
+REQ_NUM=50000000
+RANGE_NUM=20000000
+DUMP=5000
+
+echo taskset -c 1 ./redis-benchmark -n $REQ_NUM -r $RANGE_NUM -BS -RF -X $DUMP
+taskset -c 1 ./redis-benchmark -n $REQ_NUM -r $RANGE_NUM -BS -RF -X $DUMP
