ffd9d4383a85114b911a19377a3d42e80b0d4ac6
sammy <sammy@fiteen.(none)>
2010-12-16 05:37:19 -0800

    better make_test.bash

--- test/make_test.bash
+++ test/make_test.bash
@@ -3,9 +3,11 @@
 # FIRST get Alchemy's Lua Client
 git clone https://JakSprats@github.com/JakSprats/redis-lua.git
 
+# then MAKE INSTALL lua
+(cd ../lua-5.1.4/; make install)
+
 # then install luasockets
 wget http://luaforge.net/frs/download.php/2664/luasocket-2.0.2.tar.gz
 tar xvfz luasocket-2.0.2.tar.gz
-(cd luasocket-2.0.2;
-  make install
-}
+(cd luasocket-2.0.2 make install; }
+
