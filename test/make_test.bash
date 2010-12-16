43e7f7d0940dd65fd64d75a42adf1155989dcaf8
sammy <sammy@fiteen.(none)>
2010-12-16 05:40:35 -0800

    bash typos in make_test.bash

--- test/make_test.bash
+++ test/make_test.bash
@@ -9,5 +9,4 @@ git clone https://JakSprats@github.com/JakSprats/redis-lua.git
 # then install luasockets
 wget http://luaforge.net/frs/download.php/2664/luasocket-2.0.2.tar.gz
 tar xvfz luasocket-2.0.2.tar.gz
-(cd luasocket-2.0.2 make install; }
-
+(cd luasocket-2.0.2; make install; )
