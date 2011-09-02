5fac6028922a2c744d0b88488c12f44f93a05b4f
The X DB <the-x-db@mail.com>
2011-09-01 22:06:37 -0600

    refactored instance_settings.lua to config.lua

--- redis_unstable/src/SERVERS/BRIDGE/2/instance_settings.lua
+++ redis_unstable/src/SERVERS/BRIDGE/2/instance_settings.lua
@@ -1,51 +1,15 @@
-AutoIncRange = 10000;
 
-MyNodeId = -1; -- BRIDGE has no data
-NodeData = {};
-table.insert(NodeData, {ip     = "127.0.0.1",      port = 8080,
-                        domain = "www.retwis.com", synced = 0});
-table.insert(NodeData, {ip     = "127.0.0.1",      port = 8081,
-                        domain = "www.retwis.com", synced = 0});
-table.insert(NodeData, {ip     = "127.0.0.1",      port = 8082,
-                        domain = "www.retwis.com", synced = 0});
-table.insert(NodeData, {ip     = "127.0.0.1",      port = 8083,
-                        domain = "www.retwis.com", synced = 0});
-table.insert(NodeData, {ip     = "127.0.0.1",      port = 8084,
-                        domain = "www.retwis.com", synced = 0});
-table.insert(NodeData, {ip     = "127.0.0.1",      port = 8085,
-                        domain = "www.retwis.com", synced = 0});
-table.insert(NodeData, {ip     = "127.0.0.1",      port = 8086,
-                        domain = "www.retwis.com", synced = 0});
-table.insert(NodeData, {ip     = "127.0.0.1",      port = 8087,
-                        domain = "www.retwis.com", synced = 0});
+-- CONFIG CONFIG CONFIG CONFIG CONFIG CONFIG CONFIG
+dofile "../../../docroot/DIST/config.lua"
 
-MyBridgeId = 2;
-BridgeId   = 2;
-BridgeData = {};
-table.insert(BridgeData, {ip     = "127.0.0.1",      port = 20000,
-                          domain = "www.retwis.com", synced = 0});
-table.insert(BridgeData, {ip     = "127.0.0.1",      port = 20001,
-                          domain = "www.retwis.com", synced = 0});
-
-PeerData = {5, 6, 7, 8};
-
--- CONSTANT CONSTANT CONSTANT CONSTANT CONSTANT CONSTANT CONSTANT CONSTANT
-IslandData    = {};
-IslandData[1] = 1; IslandData[2] = 1;   
-IslandData[3] = 1; IslandData[4] = 1;   
-IslandData[5] = 2; IslandData[6] = 2;   
-IslandData[7] = 2; IslandData[8] = 2;  
-
-NumNodes  = #NodeData;
-NumPeers  = #PeerData;
-NumHBs    = #NodeData;
-NumToSync = #PeerData + #BridgeData - 1; -- SYNC to ALL except self
-
-MyGeneration = redis("get", "alchemy_generation");
-if (MyGeneration == nil) then MyGeneration = 0; end
-MyGeneration = MyGeneration + 1; -- This is the next generation
-redis("set", "alchemy_generation", MyGeneration);
-print('MyGeneration: ' .. MyGeneration);
+MyNodeId   = -1; -- BRIDGE has no data
+MyBridgeId =  2;
+BridgeId   =  2;
+PeerData   = {5, 6, 7, 8};
+NumNodes   = #NodeData;
+NumPeers   = #PeerData;
+NumHBs     = #NodeData;
+NumToSync  = #PeerData + #BridgeData - 1; -- SYNC to ALL except self
 
 -- APP_STACK
 dofile "../app_stack.lua"
--- redis_unstable/src/docroot/DIST/config.lua
+++ redis_unstable/src/docroot/DIST/config.lua
@@ -1,6 +1,6 @@
-AutoIncRange = 5;
 
-MyNodeId = 1;
+AutoIncRange = 10000;
+
 NodeData = {};
 table.insert(NodeData, {ip     = "127.0.0.1",      port = 8080,
                         domain = "www.retwis.com", synced = 0});
@@ -10,32 +10,31 @@ table.insert(NodeData, {ip     = "127.0.0.1",      port = 8082,
                         domain = "www.retwis.com", synced = 0});
 table.insert(NodeData, {ip     = "127.0.0.1",      port = 8083,
                         domain = "www.retwis.com", synced = 0});
+table.insert(NodeData, {ip     = "127.0.0.1",      port = 8084,
+                        domain = "www.retwis.com", synced = 0});
+table.insert(NodeData, {ip     = "127.0.0.1",      port = 8085,
+                        domain = "www.retwis.com", synced = 0});
+table.insert(NodeData, {ip     = "127.0.0.1",      port = 8086,
+                        domain = "www.retwis.com", synced = 0});
+table.insert(NodeData, {ip     = "127.0.0.1",      port = 8087,
+                        domain = "www.retwis.com", synced = 0});
 
-BridgeId   =  1;
-MyBridgeId = -1;
 BridgeData = {};
-table.insert(BridgeData, {ip     = "127.0.0.1",      port = 10000,
+table.insert(BridgeData, {ip     = "127.0.0.1",      port = 20000,
                           domain = "www.retwis.com", synced = 0});
-table.insert(BridgeData, {ip     = "127.0.0.1",      port = 10001,
+table.insert(BridgeData, {ip     = "127.0.0.1",      port = 20001,
                           domain = "www.retwis.com", synced = 0});
-
-PeerData = {1, 2, -1};
 
 -- CONSTANT CONSTANT CONSTANT CONSTANT CONSTANT CONSTANT CONSTANT CONSTANT
 IslandData    = {};
 IslandData[1] = 1; IslandData[2] = 1;   
-IslandData[3] = 2; IslandData[4] = 2;   
-
-NumNodes  = #NodeData;
-NumPeers  = #PeerData - 1; -- no bridge
-NumToSync = #PeerData - 1; -- no self
+IslandData[3] = 1; IslandData[4] = 1;   
+IslandData[5] = 2; IslandData[6] = 2;   
+IslandData[7] = 2; IslandData[8] = 2;   
 
 MyGeneration = redis("get", "alchemy_generation");
 if (MyGeneration == nil) then MyGeneration = 0; end
 MyGeneration = MyGeneration + 1; -- This is the next generation
 redis("set", "alchemy_generation", MyGeneration);
 print('MyGeneration: ' .. MyGeneration);
-
--- WHITELISTED_FUNCTIONS WHITELISTED_FUNCTIONS WHITELISTED_FUNCTIONS
-dofile "./docroot/DIST/whitelist.lua";
 
