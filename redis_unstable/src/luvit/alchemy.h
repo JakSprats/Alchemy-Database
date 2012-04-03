adf8b0ae683a2cde0b4b62bb3e710a8f51a8cffd
The X DB <the-x-db@mail.com>
2012-04-03 14:20:10 -0300

    CLEANUP: luvit

--- redis_unstable/src/luvit/alchemy.h
+++
@@ -1,9 +0,0 @@
-#ifndef LIB_ALCHEMY
-#define LIB_ALCHEMY
-
-#include "lua.h"
-#include "lauxlib.h"
-
-LUALIB_API int luaopen_alchemy(lua_State *L);
-
-#endif /* LIB_ALCHEMY */
--- redis_unstable/src/luvit/testit.lua
+++
@@ -1,5 +0,0 @@
--- Load our native module
-local alchemy = require('./alchemy')
-alchemy.call("SET", "TEST_KEY", "PURE_LUA");
-local res = alchemy.call("GET", "TEST_KEY");
-print('GET: res: ' .. res);
