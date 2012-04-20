2376ae2860c6974b5614821caa32dd00e1953a32
The X DB <the-x-db@mail.com>
2012-04-20 15:33:57 -0300

    REDIS 2.6: backed out for now

--- redis_2.6/tests/unit/introspection.tcl
+++
@@ -1,5 +0,0 @@
-start_server {tags {"introspection"}} {
-    test {CLIENT LIST} {
-        r client list
-    } {*addr=*:* fd=* age=* idle=* flags=N db=9 sub=0 psub=0 qbuf=0 qbuf-free=* obl=0 oll=0 omem=0 events=r cmd=client*}
-}
